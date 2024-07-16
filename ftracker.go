16bc6d083ce1b2ac63b1d4629e87fdd67426da3a
MaxA1993 <paykxzxz@yandex.ru>
2024-07-16 13:34:30 +0300

    AndreevSprintFourFinalTask

--- ftracker.go
+++ ftracker.go
@@ -51,7 +51,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 	case trainingType == "Бег":
 		distance := distance(action)
 		speed := meanSpeed(action, duration)
-		calories := RunningSpentCalories(action, duration, weight)
+		calories := RunningSpentCalories(action, weight, duration)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Ходьба":
 		distance := distance(action)
@@ -60,8 +60,8 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	case trainingType == "Плавание":
 		distance := distance(action)
-		speed := meanSpeed(action, duration)
-		calories := SwimmingSpentCalories(lengthPool, countPool, weight, duration)
+		speed := swimmingMeanSpeed(lengthPool, countPool, duration)
+		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight)
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
 	default:
 		return "неизвестный тип тренировки"
@@ -84,7 +84,7 @@ const (
 func RunningSpentCalories(action int, weight, duration float64) float64 {
 	// ваш код здесь
 	meanSpeed := meanSpeed(action, duration)
-	calories := (runningCaloriesMeanSpeedMultiplier * meanSpeed * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * minInH
+	calories := ((runningCaloriesMeanSpeedMultiplier * meanSpeed * runningCaloriesMeanSpeedShift) * weight / mInKm * duration * minInH)
 	return calories
 }
 
@@ -104,8 +104,9 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	meanSpeed := meanSpeed(action, duration) * mInKm / (minInH * secInM)
-	calories := (walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed*kmhInMsec, 2)/height)*walkingSpeedHeightMultiplier) * duration * minInH
+	meanSpeed := meanSpeed(action, duration) * kmhInMsec
+	height /= cmInM
+	calories := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed, 2)/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 
 	return calories
 }
